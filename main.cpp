23591bd84207b1949ee4d67168fab6b97a267b2a
ikram-salahudeen <ikramsaladin@icloud.com>
2025-02-13 18:36:07 +0000

    13_2

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 #include "pins.h"
 
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE_MS                                                    500
+constexpr int PWM_FREQUENCY = 30000;
+
 
 
 int main()
 {
-    Motor L(MOTOR_L_PWM, MOTOR_L_DIR, MOTOR_L_BIPOLAR, ENCODER_L_1, ENCODER_L_2, ENCODER_L_IDX, 1000);
-    Motor R(MOTOR_R_PWM, MOTOR_R_DIR, MOTOR_R_BIPOLAR, ENCODER_R_1, ENCODER_R_2, ENCODER_R_IDX, 1000);
+    Motor L(MOTOR_L_PWM, MOTOR_L_DIR, MOTOR_L_BIPOLAR, ENCODER_L_A, ENCODER_L_B, NC, PWM_FREQUENCY);
+    Motor R(MOTOR_R_PWM, MOTOR_R_DIR, MOTOR_R_BIPOLAR, ENCODER_R_A, ENCODER_R_B, NC, PWM_FREQUENCY);
 
     DigitalOut en (ENABLE);
     en.write(1);
@@ -22,13 +22,13 @@ int main()
     L.pwm.write(0.5f);
     R.direction.write(1);
     R.bipolar.write(1);
-    R.pwm.write(0.4f);
+    R.pwm.write(0.5f);
 
     printf("Enc");
     while(true) {
          printf("%i %i\n", (int) L.encoder.getPulses(), (int) R.encoder.getPulses());
          wait(1);
-    }
+    };
 
    
 
--- pins.h
+++ pins.h
@@ -1,21 +1,25 @@
 #include "mbed.h"
 
-#define MOTOR_L_BIPOLAR PB_4
-#define MOTOR_L_DIR PB_3
-#define MOTOR_L_PWM PA_8
-#define MOTOR_L_CUR_SENSE1 PA_9
-#define MOTOR_L_CUR_SENSE2 PC_7
-#define ENCODER_L_1 PA_1
-#define ENCODER_L_2 PA_0
-#define ENCODER_L_IDX NC
+#define MOTOR_L_BIPOLAR PC_0
+#define MOTOR_L_DIR PC_1
+#define MOTOR_L_PWM PB_0
 
-#define MOTOR_R_BIPOLAR PB_5
-#define MOTOR_R_DIR PA_10
-#define MOTOR_R_PWM PB_10
-#define MOTOR_R_CUR_SENSE2 PA_7
-#define MOTOR_R_CUR_SENSE1 PA_6
-#define ENCODER_R_1 PC_1
-#define ENCODER_R_2 PC_0  bv 
-#define ENCODER_R_IDX NC
+#define MOTOR_R_BIPOLAR PA_4
+#define MOTOR_R_DIR PA_1
+#define MOTOR_R_PWM PA_0
 
-#define ENABLE PB_6
+#define ENABLE PA_2
+
+#define MICROLAN
+
+#define MOTOR_L_CUR_SENSE1
+#define MOTOR_L_CUR_SENSE2
+#define MOTOR_R_CUR_SENSE2
+#define MOTOR_R_CUR_SENSE1
+
+
+#define ENCODER_L_A PA_2
+#define ENCODER_L_B PA_10 
+
+#define ENCODER_R_A PB_5
+#define ENCODER_R_B PB_4
